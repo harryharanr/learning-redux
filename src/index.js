73c820af8fb38e1616fd6a9e42b3b123eef5bd5f
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-16 12:31:03 +0530

    actionCreators.js created

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import store from "./store";
 import * as actions from "./actionTypes";
+import { bugAdded } from "./actionCreators";
 /* 
   subscribe() method returns a function to unsubscribe
 */
-const unsubscribe = store.subscribe(() => {
+store.subscribe(() => {
   // Whenever store gets updated , below console.log would be triggered
   console.log("Store changed !!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug 1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
 
-unsubscribe();
-// Below change will not be notified to the subscription
+/* Using actionCreators , we need not write the same code when the same action
+  is used somewhere else . we can simple call the action creator function and pass the necessary 
+  parameter(s) */
 store.dispatch({
   type: actions.BUG_REMOVED,
   payload: {
