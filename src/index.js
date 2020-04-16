1b1d0fdd1b6b27840344537f994fe8e49fea82e5
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-16 20:09:32 +0530

    ducks pattern created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import store from "./store";
-import * as actions from "./actionTypes";
-import { bugAdded, bugRemoved, bugResolved } from "./actionCreators";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
 
+const store = configureStore();
 /* 
   subscribe() method returns a function to unsubscribe
 */
@@ -15,8 +15,10 @@ store.subscribe(() => {
   is used somewhere else . we can simple call the action creator function and pass the necessary 
   parameter(s) */
 
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugResolved(1));
-store.dispatch(bugRemoved(1));
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugRemoved(1));
 
 console.log("Final State", store.getState());
