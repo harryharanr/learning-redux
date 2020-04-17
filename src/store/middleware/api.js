e85432d15c2e02f9305547ddee7ef09ccbc5b0c4
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-18 01:15:48 +0530

    used action creators

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-
+import * as actions from "../api";
 const api = ({ dispatch }) => (next) => async (action) => {
-  if (action.type !== "apiCallBegan") return next(action);
+  if (action.type !== actions.apiCallBegan.type) return next(action);
 
   const { url, method, data, onSuccess, onError } = action.payload;
 
+  next(action);
   try {
     const response = await axios.request({
       baseURL: "http://localhost:9001/api",
@@ -12,9 +13,16 @@ const api = ({ dispatch }) => (next) => async (action) => {
       method,
       data,
     });
-    dispatch({ type: onSuccess, payload: response.data });
+
+    // General success action
+    dispatch(actions.apiCallSuccess(response.data));
+    // Specific success action
+    if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
   } catch (error) {
-    dispatch({ type: onError, payload: error });
+    // General error action
+    dispatch(actions.apiCallFailed(error));
+    // Specific error action
+    if (onError) dispatch({ type: onError, payload: error });
   }
 };
 
