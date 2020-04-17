066cc419c957fac0c6ee92ff4c6009c54d9ac6a4
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-18 03:33:22 +0530

    caching added

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
 import { apiCallBegan } from "./api";
+import moment from "moment";
 
 let lastId = 0;
 
@@ -19,6 +20,7 @@ const slice = createSlice({
     bugsReceived: (bugs, action) => {
       bugs.list = action.payload;
       bugs.loading = false;
+      bugs.lastFetch = Date.now();
     },
     bugsRequestFailed: (bugs, action) => {
       bugs.loading = false;
@@ -57,13 +59,21 @@ export default slice.reducer;
 // Actions creators
 const url = "/bugs";
 
-export const loadBugs = () =>
-  apiCallBegan({
-    url,
-    onStart: bugsRequested.type,
-    onSuccess: bugsReceived.type,
-    onError: bugsRequestFailed.type,
-  });
+export const loadBugs = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.bugs;
+
+  const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
+  console.log(diffInMinutes);
+  if (diffInMinutes < 10) return;
+  dispatch(
+    apiCallBegan({
+      url,
+      onStart: bugsRequested.type,
+      onSuccess: bugsReceived.type,
+      onError: bugsRequestFailed.type,
+    })
+  );
+};
 
 // Selector
 
